146ba335c9a6909e6217eb9bd97e9812f8ef150e
ryoctrl <ryoctrl@gmail.com>
2022-08-09 05:07:56 +0900

    feat(top): add support for ogp on top page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import { HomeHeader } from "@components/molecules";
 import { HomeLayout } from "@components/organisms";
 import { useWorks } from "hooks/works/useWorks";
 import { useExhibitions } from "hooks/exhibitions/use-exhibitions";
+import OGP from "@components/organisms/ogp";
 
 const Home: NextPage = () => {
   const { worksState } = useWorks();
@@ -22,6 +23,12 @@ const Home: NextPage = () => {
           content={currentExhibition?.title ?? "KUMD海賊版パネル展示会"}
         />
       </Head>
+      <OGP
+        pageTitle={`${currentExhibition?.title} ?? "KUMD海賊版パネル展示会"`}
+        pagePath={`https://mirin-app-2022.mosin.jp`}
+        pageDescription={currentExhibition?.title ?? "KUMD海賊版パネル展示会"}
+        pageImg={currentExhibition?.heroImage?.pc}
+      />
 
       <main className={styles.main}>
         <HomeHeader />
