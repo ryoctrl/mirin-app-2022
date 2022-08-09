eb6dd1636a83dcdf77dc71329d20b3821dacbf6d
ryoctrl <ryoctrl@gmail.com>
2022-08-09 19:58:50 +0900

    feat(functions): add support for sendgrid

--- functions/src/user.ts
+++ functions/src/user.ts
@@ -1,5 +1,16 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as SendGrid from "@sendgrid/mail";
+
+SendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+const mailTemplate = `
+<h1>KUMD管理者ページへの招待</h1>
+
+<a href="{{LINK}}">このリンク</a> をクリックしてパスワードの設定を完了し、ログインしてください。
+
+以後は <a href="{{ADMIN_LINK}}">管理者ページ</a> よりログインしてください。
+`;
 
 export const registerNewUser = functions.https.onCall(async (data, context) => {
   if (!context.auth?.token.admin) {
@@ -9,9 +20,46 @@ export const registerNewUser = functions.https.onCall(async (data, context) => {
     );
   }
 
-  const newUser = await admin.auth().createUser({
-    email: data.email,
-  });
+  const newUser = await admin
+    .auth()
+    .createUser({
+      email: data.email,
+    })
+    .catch((err) => {
+      return err;
+    });
+
+  if (newUser instanceof Error) {
+    throw new functions.https.HttpsError("internal", newUser.message);
+  }
+
+  const adminUrl = process.env.SITE_URL.endsWith("/")
+    ? `${process.env.SITE_URL}admin`
+    : `${process.env.SITE_URL}/admin`;
+
+  const url = await admin
+    .auth()
+    .generatePasswordResetLink(data.email, {
+      url: adminUrl,
+    })
+    .catch((err) => err);
+  if (url instanceof Error) {
+    throw new functions.https.HttpsError("internal", newUser.message);
+  }
+
+  const message = {
+    to: data.email,
+    from: "noreply@kumd.mosin.jp",
+    subject: "KUMD管理者ページへの招待",
+    html: mailTemplate
+      .replace(/{{LINK}}/g, url)
+      .replace(/{{ADMIN_LINK}}/g, adminUrl),
+  };
+
+  const result = await SendGrid.send(message).catch((err) => err);
+  if (result instanceof Error) {
+    throw new functions.https.HttpsError("internal", newUser.message);
+  }
 
   return {
     user: {
