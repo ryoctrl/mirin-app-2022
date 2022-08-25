358514f00607495876ab75dd3247916258a11dab
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2022-08-26 01:49:51 +0900

    ヘッダー画像周りの修正とタイトル・スクロールバーの追加

--- components/molecules/home/header.tsx
+++ components/molecules/home/header.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import { useExhibitions } from "hooks/exhibitions/use-exhibitions";
+import { HeaderScrollRefs, scroll } from "libs/utils/header";
 
 export const HomeHeader: React.FC = () => {
   const {
@@ -18,14 +19,25 @@ export const HomeHeader: React.FC = () => {
         />
       )}
       {currentExhibition?.heroImage.sp && (
-        <Image
-          alt={currentExhibition.title}
-          src={currentExhibition.heroImage.sp}
-          className="visible md:invisible"
-          layout="fill"
-          objectFit="cover"
-        />
+        <div>
+          <div className="header-title">
+            <h2>漫画同好会</h2>
+            <h2>海賊版</h2>
+            <h2>パネル展示会</h2>
+          </div>
+          <Image
+            alt={currentExhibition.title}
+            src={currentExhibition.heroImage.sp}
+            className="visible md:invisible"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
       )}
+      <div className="scroll-header-bar" onClick={() => scroll(HeaderScrollRefs.ABOUT)}>
+        <div className="bar"></div>
+        <p>scroll</p>
+      </div>
     </div>
   );
 };
